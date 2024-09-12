523ec6d5de32172efacfbff86cb311e906b7ab35
HyunseungJeong <hs.jeong@rnadatasystem.com>
2024-09-12 17:34:18 +0900

    오류 수정

--- src/main/java/egovframework/com/cmm/interceptor/BusinessInterceptor.java
+++ src/main/java/egovframework/com/cmm/interceptor/BusinessInterceptor.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
+import com.rds.adams.web.common.AdamsConstant;
 import com.rds.adams.web.common.login.dto.AdamsMenuDTO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,26 +34,41 @@ public class BusinessInterceptor extends WebContentInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		
+		String uri = request.getRequestURI();
 		log.info("========================================================");
-		log.info(" BusinessInterceptor [URI : "+request.getRequestURI()+"]");
+		log.info(" BusinessInterceptor [URI : "+uri+"]");
 		
 		HttpSession session = request.getSession();
-		List<AdamsMenuDTO> menuList = (List<AdamsMenuDTO>) session.getAttribute("MenuTreeVOList");
+		List<AdamsMenuDTO> menuList = (List<AdamsMenuDTO>) session.getAttribute(AdamsConstant.SESSION_MENU_TREELIST);
 		
-		String menuId = "";
+		String menuId = getMenuIdByUrl(uri, request.getSession());
 
 		if (modelAndView != null) {
 			modelAndView.addObject("menuList", menuList);
+			modelAndView.addObject("menuId", menuId);
 		}
 		
 		log.info("========================================================");
 		
 	}
 	
-	private String getMenuIdByUrl(String url, HttpSession session) {
+	private String getMenuIdByUrl(String uri, HttpSession session) {
 		
-		List<AdamsMenuDTO> menuList = (List<AdamsMenuDTO>) session.getAttribute("MenuTreeVOList");
-		return null;
+		List<AdamsMenuDTO> menuList = (List<AdamsMenuDTO>) session.getAttribute(AdamsConstant.SESSION_MENU_FLATLIST);
+		String menuId = "";
+		
+		if (menuList == null) {
+			return "";
+		} else {
+			for (AdamsMenuDTO menuDTO : menuList) {
+				if (uri.equals(menuDTO.getPgmUrl())) {
+					menuId = menuDTO.getMenuId();
+				}
+			}
+		}
+		
+		return menuId;
+		
 	}
 
 }
