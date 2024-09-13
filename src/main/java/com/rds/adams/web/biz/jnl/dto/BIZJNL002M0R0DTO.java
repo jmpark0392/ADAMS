1ba0c16158b8236422a69d368dbbac80f06bbfc8
KimNaJeong <nj.kim@rnadatasystem.com>
2024-09-13 17:49:49 +0900

    수정 반영

--- src/main/java/com/rds/adams/web/biz/jnl/dto/BIZJNL002M0R0DTO.java
+++ src/main/java/com/rds/adams/web/biz/jnl/dto/BIZJNL002M0R0DTO.java
@@ -23,13 +23,16 @@ import lombok.ToString;
 @Setter
 public class BIZJNL002M0R0DTO {
 	
+	private int rowNumber; 
 	private String inscDvCd;  /* 보험계약구분코드 */
-	private String valMetdCd; /* 평가방법코드 */
+	private String valuMetdCd; /* 평가방법코드 */
 	private String lvl1Cd;  /* 레벨1 */
 	private String lvl2Cd;  /* 레벨2 */
 	private String lvl3Cd;  /* 레벨3 */
 	private String drAcSubjCd;  /* 차변계정코드 */
+	private String drAcSubjNm;  /* 차변계정명 */
 	private String crAcSubjCd;  /* 대변계정코드 */
+	private String crAcSubjNm;  /* 대변계정명 */
 	private String etcDesc;  /* 기타내용 */
 	private String useYn;  /* 사용여부 */
 	private String regUsrid;  /* 등록사용자ID */
