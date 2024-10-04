321c6258676360253a769e310a2266338c8e208b
changgi <cg.lee@rnadatasystem.com>
2024-10-04 16:36:59 +0900

    update commit

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -378,8 +378,9 @@ function validateEmail(email) {
 
 
 /*===========================================
- * 이메일 형식을 검증하는 함수
- * @param obj   Object
+ * ajax 호출용 공통 Transaction 함수
+ * @param option   Object
+ * @param callbackFn   Function
  * @return true : true
  ===========================================*/
 function gf_Transaction(option, callbackFn ) {
@@ -448,4 +449,88 @@ function gf_Transaction(option, callbackFn ) {
 		                }
   });
   
+}
+
+
+/*===========================================
+ * 그리드 select Editor 목록에 빈값 생성
+ * @param obj   Object
+ * @return true : true
+ ===========================================*/
+function gf_PopulateSelect(select, dataSource, addBlank) {
+     var index, len, newOption;
+     if (addBlank) { select.appendChild(new Option('', '')); }
+     $.each(dataSource, function (value, text) {
+          newOption = new Option(text, value);
+         select.appendChild(newOption);
+    });
+}
+/*===========================================
+ * 그리드 select Editor 목록 생성
+ * @param obj   Object
+ * @return true : true
+ ===========================================*/
+function gf_Select2Editor(args) {
+    var $input;
+    var defaultValue;
+    var scope = this;
+    var calendarOpen = false;
+    this.keyCaptureList = [ Slick.keyCode.UP, Slick.keyCode.DOWN, Slick.keyCode.ENTER ];
+    this.init = function () {
+        $input = $('<select></select>');
+        $input.width(args.container.clientWidth + 3);
+        gf_PopulateSelect($input[0], args.column.dataSource, true);
+        $input.appendTo(args.container);
+        $input.focus().select();
+
+        $input.select2({
+            placeholder: '-',
+            allowClear: true
+        });
+    };
+    this.destroy = function () {
+        $input.select2('close');
+        $input.select2('destroy');
+        $input.remove();
+    };
+    this.show = function () {
+    };
+    this.hide = function () {
+    };
+    this.position = function (position) {
+    };
+    this.focus = function () {
+        $input.select2('input_focus');
+    };
+    this.loadValue = function (item) {
+        defaultValue = item[args.column.field];
+        $input.val(defaultValue);
+        $input[0].defaultValue = defaultValue;
+        $input.trigger("change.select2");
+    };
+    this.serializeValue = function () {
+        return $input.val();
+    };
+    this.applyValue = function (item, state) {
+        item[args.column.field] = state;
+    };
+    this.isValueChanged = function () {
+        return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+    };
+    this.validate = function () {
+        return {
+            valid: true,
+            msg: null
+        };
+    };
+    this.init();
+}
+ 
+/*===========================================
+ * 그리드 select 콤보박스 포멧
+ * @param obj   Object
+ * @return true : true
+ ===========================================*/
+function gf_Select2Formatter(row, cell, value, columnDef, dataContext) {
+    return columnDef.dataSource[value] || '-';
 }
