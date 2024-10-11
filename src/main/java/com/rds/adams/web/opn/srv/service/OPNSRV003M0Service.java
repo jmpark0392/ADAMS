787658b19731124666bc9a471a8bd6d6bf6614e9
sardormv <sardor@rnadatasystem.com>
2024-10-11 17:44:20 +0900

    feat: opn service logic initialized

---
+++ src/main/java/com/rds/adams/web/opn/srv/service/OPNSRV003M0Service.java
@@ -0,0 +1,31 @@
+package com.rds.adams.web.opn.srv.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rds.adams.web.opn.srv.dao.OPNSRV003M0DAO;
+import com.rds.adams.web.opn.srv.dto.OPNSRV003M0P0DTO;
+import com.rds.adams.web.opn.srv.dto.OPNSRV003M0R0DTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+
+public class OPNSRV003M0Service  {
+    @Autowired
+    private OPNSRV003M0DAO opnsrv003m0dao;
+
+    public List<OPNSRV003M0R0DTO> getChartInfos(OPNSRV003M0P0DTO inVo) {
+		
+        List<OPNSRV003M0R0DTO> opnsrv003m0r0dtos = opnsrv003m0dao.getChartInfos(inVo);
+		
+        log.debug("opnsrv003m0r0dtos : " + opnsrv003m0r0dtos.toString());
+		
+        return opnsrv003m0r0dtos;
+    }
+}
