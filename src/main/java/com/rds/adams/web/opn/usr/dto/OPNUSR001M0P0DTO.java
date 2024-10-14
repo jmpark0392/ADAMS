4f50adda6786cab1bf799d6fff992c4e3a928e57
ByungSunBae <bs.bae@rnadatasystem.com>
2024-10-14 17:29:50 +0900

    메뉴관리 화면 개발

---
+++ src/main/java/com/rds/adams/web/opn/usr/dto/OPNUSR001M0P0DTO.java
@@ -0,0 +1,39 @@
+package com.rds.adams.web.opn.usr.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * RDS STANDARD DEVELOP FRAMEWORK
+ *
+ * @since  : 2024. 10. 11.
+ * @author : BAE BYUNGSUN
+ * E-MAIL  : bs.bae@rnadatasystem.com
+ * <PRE>
+ * 개정이력
+ * ----------------------------------------------------------
+ * 2024-10-11 : 최초 등록
+ * ----------------------------------------------------------
+ * </PRE>
+ */
+@Schema(description = "메뉴 조회 조건 DTO")
+@ToString
+@Getter
+@Setter
+public class OPNUSR001M0P0DTO {
+
+    @Schema(description = "메뉴명 한글")
+    private String menuNmKor;
+    
+    @Schema(description = "메뉴명 영문")
+    private String menuNmEng;
+    
+    @Schema(description = "메뉴 설명")
+    private String menuDesc;
+    
+    @Schema(description = "프로그램명")
+    private String pgmNm;
+
+}
