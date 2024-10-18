10eaae10a0c572d38efeca41a875f9cca8d5e242
jm.park <jm.park@rnadatasystem.com>
2024-10-18 18:26:38 +0900

    Modify ADMUSR001

--- src/main/java/com/rds/adams/web/adm/usr/dto/ADMUSR001M0R0DTO.java
+++ src/main/java/com/rds/adams/web/adm/usr/dto/ADMUSR001M0R0DTO.java
@@ -26,50 +26,73 @@ import lombok.ToString;
 @Setter
 public class ADMUSR001M0R0DTO {
 
-    @Schema(description = "고객사번호")
-    private String csNo;
-	
-    @Schema(description = "국가명")
-    private String CountryNmEng;
+	   @Schema(description = "고객사번호")
+	    private String csNo;
 
-    @Schema(description = "회사번호구분코드")
-    private int CompNoDvCd;
-
-    @Schema(description = "회사번호")
-    private String CompNo;
-	
-    @Schema(description = "회사명")
-    private String CompNm;
-	
-    @Schema(description = "대표자명")
-    private String ReperNm;
-
-    @Schema(description = "대표전화번호")
-    private String RepPhNo;
-
-    @Schema(description = "담당자명")
-    private String PtbNm;
-
-    @Schema(description = "담당자전화번호")
-    private String PtbPhNo;
-	
-	@Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
-    @Schema(description = "담당자이메일")
-    private String PtbEmail;
-
-    @Schema(description = "우편번호")
-    private String PostNo;
-
-    @Schema(description = "기본주소")
-    private String Addrs;
-
-    @Schema(description = "상세주소")
-    private String DtlsAddrs;
-
-    @Schema(description = "사용시작일시")
-    private String UseStrDtm;
-
-    @Schema(description = "최초등록일시")
-    private String FrstRegDtm;
-
+		@Schema(description = "회사명")
+	    private String compNm;
+		
+		@Schema(description = "회사구분코드")
+	    private String compNoDvCd;
+		
+		@Schema(description = "회사번호")
+	    private String CompNo;
+		
+		@Schema(description = "국가명영문")
+	    private String countryNmEng;
+		
+		@Schema(description = "국가명한글")
+	    private String countryNmKor;
+		
+	    @Schema(description = "회사홈페이지")
+	    private String compHmpgUrl;
+	    
+	    @Schema(description = "서비스사용코드")
+	    private String statDvCd;
+		
+		@Schema(description = "기본주소")
+	    private String addrs;
+		
+		@Schema(description = "상세주소")
+	    private String dtlsAddrs;
+		
+		@Schema(description = "우편번호")
+	    private String postNo;
+		
+		@Schema(description = "대표자명")
+	    private String reperNm;
+		
+		@Schema(description = "대표전화번호")
+	    private String repPhNo;
+		
+		@Schema(description = "담당자명")
+	    private String ptbName;
+		
+		@Schema(description = "당당자전화번호")
+	    private String ptbPhNo;
+		
+		@Schema(description = "당당자이메일")
+	    private String ptbEmail;
+		
+		@Schema(description = "사용종료일시")
+	    private String useEndDtm;
+		
+		@Schema(description = "사용시작일시")
+	    private String useStrDtm;
+		
+		@Schema(description = "최초요청사용자")
+	    private String frstRegEmpNo;
+		
+		@Schema(description = "최초요청일시")
+	    private String FrstRegDtm;
+		
+		@Schema(description = "국가코드")
+	    private String countryCd;
+		
+		@Schema(description = "최종업데이트사용자")
+	    private String fnlUpdEmpNo;
+		
+		@Schema(description = "최종업데이트일시")
+	    private String fnlUpdDtm;
+		
 }
