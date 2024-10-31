b79945c5f4cec5a82bd5421370c2967809749e4c
changgi <cg.lee@rnadatasystem.com>
2024-10-31 16:54:04 +0900

    update commit

--- src/main/java/com/rds/adams/web/biz/ins/service/BIZINS001M0Service.java
+++ src/main/java/com/rds/adams/web/biz/ins/service/BIZINS001M0Service.java
@@ -26,20 +26,25 @@ public class BIZINS001M0Service {
 	
 	public void executeList(ExecuteDTO inVo) {
 
-
-		inVo.setBatProgId("B0001");
-		inVo.setBatExeRstCd("");
-		inVo.setBatExeErrCd("");
-		inVo.setBatLoadStatCd("1");
-		
-		executeResultDAO.insertBatHist(inVo);
-		
-		bIZINS001M0DAO.deleteListBf(inVo);
-		bIZINS001M0DAO.insertList(inVo);
-		
-		inVo.setBatLoadStatCd("0");
-		inVo.setBatExeRstCd("OK");
-		executeResultDAO.updateBatHist(inVo);
+		try {
+			inVo.setBatProgId("B0001");
+			inVo.setBatExeRstCd("");
+			inVo.setBatExeErrCd("");
+			inVo.setBatLoadStatCd("1");
+			
+			executeResultDAO.insertBatHist(inVo);
+			
+			bIZINS001M0DAO.deleteListBf(inVo);
+			bIZINS001M0DAO.insertList(inVo);
+			
+			inVo.setBatLoadStatCd("0");
+			inVo.setBatExeRstCd("OK");
+			executeResultDAO.updateBatHist(inVo);
+		} catch ( Exception e ) {
+			inVo.setBatLoadStatCd("9");
+			inVo.setBatExeRstCd("ERROR");
+			executeResultDAO.updateBatHist(inVo);
+		}
 		
 	return;
 	}
